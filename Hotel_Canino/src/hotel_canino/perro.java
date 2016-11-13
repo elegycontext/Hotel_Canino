345d055df6465b22555d51cb8366792eadffbacc
elegycontext <apotatohat@gmail.com>
2016-11-13 18:25:27 -0400

    se añadio metodo agregar + comentarios

--- Hotel_Canino/src/hotel_canino/perro.java
+++ Hotel_Canino/src/hotel_canino/perro.java
@@ -6,19 +6,27 @@
 package hotel_canino;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class perro {
-        String fecha, raza, color, nombre, edad, dueño, ci, datos;
+      static String  raza, color, nombre, edad, dueño, ci; //todo es estatico para que no se borre al final del metodo//
+      static String datos, datos2;
+      static int year, month, day;
     
-    void agregar() throws IOException{
+   static void agregar() throws IOException{
         /*
             Dentro del bloque del try se solicitan todos los datos, en caso de 
             en caso de que se genere algun error lo muestra y sale.
         */
     BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
     try{
-    System.out.print("Ingrese fecha:");
-    fecha = br.readLine();
+    Date date = new Date();
+    LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    year = localDate.getYear();
+    month = localDate.getMonthValue();
+    day = localDate.getDayOfMonth();
     
     System.out.print("Ingrese raza:");
     raza = br.readLine();
@@ -45,8 +53,53 @@ public class perro {
     /*
         La variable datos guarda todos los datos tanto del perro como de su dueño
     */
-    datos = "Fecha de ingreso: " + fecha + "\n" + "Raza: " + raza + "\n" + 
-            "Color: " + color + "\n" + "Nombre: " + nombre + "\n" + "Edad: " + 
+    datos = "Fecha de ingreso: " + day +"/" + month + "/"+ year+ 
+            "\n" + "Raza: " + raza + "\n" + "Color: " + color + 
+            "\n" + "Nombre: " + nombre + "\n" + "Edad: " + 
             edad + "\n" + "Dueño: " + dueño + "\n" + "Cedula: " + ci;
+    }
+    
+    void modificar (String menu) throws IOException{
+        datos2 = datos; // para que datos2 comience con la misma informacion que se ingreso en el comienzo//
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        try {
+            switch (menu.toLowerCase()){  //dependiendo de la opcion deseada, se realiza el cambio//
+            case "raza":
+                System.out.print("Ingrese la nueva raza: ");
+                raza = br.readLine();
+                break;
+            case "color":
+                System.out.print("Ingrese el nuevo color: ");
+                color = br.readLine();
+                break;
+            case "nombre":
+                System.out.print("ingrese el nuevo nombre: ");
+                nombre = br.readLine();
+                break;
+            case "edad":
+                System.out.print("ingrese la nueva edad: ");
+                edad = br.readLine();
+                break;
+            case "dueño":
+                System.out.print("ingrese su nuevo dueño: ");
+                dueño = br.readLine();
+                break;
+            case "cedula":
+                System.out.print("ingrese la nueva cedula: ");
+                ci = br.readLine();
+                break;
+            default:
+                System.out.print("Esta opcion no es valida: ");
+            }
+        }
+        catch(Exception e){
+        System.err.println("Se produjo un Error: " + e.getMessage());
+        }
+        
+        datos2 = "Fecha de ingreso: " + day +"/" + month + "/"+ year+ 
+            "\n" + "Raza: " + raza + "\n" + "Color: " + color + 
+            "\n" + "Nombre: " + nombre + "\n" + "Edad: " + 
+            edad + "\n" + "Dueño: " + dueño + "\n" + "Cedula: " + ci; 
+            // ahora tiene la informacion ingresada mas la modificada//
     }
 }
