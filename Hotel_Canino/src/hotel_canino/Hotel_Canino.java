e61f8e4ffe742fbf860f9ee7cd1ae04315f198c1
Samy Mahmod <symq9485@gmail.com>
2016-11-13 02:00:50 -0400

    Programa terminado

--- Hotel_Canino/src/hotel_canino/Hotel_Canino.java
+++ Hotel_Canino/src/hotel_canino/Hotel_Canino.java
@@ -1,21 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hotel_canino;
 
-/**
- *
- * @author symq9485
- */
+import java.io.*;
+
 public class Hotel_Canino {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) throws IOException {
     
+        String continuar = "s";
+        String lista[];             //Se crea el arreglo
+        lista = new String[5];
+
+        String opcion;      //variable de opciones
+        
+        int i = 0, j;           //variables de contadores
+        String ver;
+
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        
+        //for (int l=0 ; l<=4;l++){
+         //   lista[l]="vacio";
+        //}
+
+        while ("s".equals(continuar)){
+            
+            try{
+            System.out.println("Que operacion desea realizar:\nA.- Agregar.\nB.- Modificar.\nC.- Eliminar.\nD.- Ninguna.");
+            opcion = br.readLine();
+
+            if (opcion.equals("a")){
+               perro agregar = new perro();
+               agregar.agregar();
+               lista[i]= agregar.datos;
+               i++;
+            }
+            else{
+                if ("b".equals(opcion)){
+                    System.out.print("Ingrese el numero que desea modificar(0-4):");
+                    j = Integer.parseInt(br.readLine());
+                    perro modificar = new perro();
+                    modificar.agregar();
+                    lista[j]=modificar.datos;
+                }
+                else{
+                    if (opcion.equals("c")){
+                    System.out.print("Ingrese el numero que desea eliminar(0-4):");
+                    j = Integer.parseInt(br.readLine());
+                    lista[j]=null;
+                    }
+                    else{
+                            continuar = "n";
+                        }
+                }
+            }
+            System.out.print("Desea continuar(s/n):");
+            continuar = br.readLine();
+            }
+            catch(IOException | NumberFormatException e){
+                System.err.println("Se produjo un Error: " + e.getMessage() + ".Por favor vuelva a ingresar los datos");
+            }
+        }        
+        System.out.print("Ver la lista(s/n):");
+        ver = br.readLine();
+        
+        if ("s".equals(ver)){
+                for (int z = 0;z <= 4;z++){
+                    System.out.println(lista[z]);
+                }
+            }
+    }
 }
